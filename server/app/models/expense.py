7095f929ef1f2313105d38200c3d3f20c20e348f
tukto12 <pawewoj214@gmail.com>
2024-11-11 14:32:41 +0100

    Delete redundant imports

--- server/app/models/expense.py
+++ server/app/models/expense.py
@@ -1,7 +1,5 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Float, Boolean
+from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.orm import relationship
-from sqlalchemy.sql import func
-from sqlalchemy_utils import EmailType
 from app.db.db_config import Base
 
 
--- server/app/models/imports.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Float, Boolean
-from sqlalchemy.orm import relationship
-from sqlalchemy.sql import func
-from sqlalchemy_utils import EmailType
-from app.db.db_config import Base
