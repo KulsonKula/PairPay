c52e081dd5c850a25175d40cd9081ef58bdbabb7
Kuba <kulajakub196@gmail.com>
2024-11-22 13:00:37 +0100

    sketch of log_wrapper

--- server/app/utils/helpers.py
+++ server/app/utils/helpers.py
@@ -1,3 +1,23 @@
+
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def log_wrapper(func):
+    def wrapper(*args, **kwargs):
+        logging.info(
+            f"Wywołanie funkcji '{func.__name__}' z argumentami: {args}, {kwargs}")
+
+        result = func(*args, **kwargs)
+
+        logging.info(f"Funkcja '{func.__name__}' zakończona. Wynik: {result}")
+
+        return result
+    return wrapper
+
+
 def serialize_bill(bill):
     return {
         "id": bill.id,
