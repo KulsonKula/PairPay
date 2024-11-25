5f9bb9a94d8f2abdad4b02e0f27db240e21f8f41
tukto12 <pawewoj214@gmail.com>
2024-11-25 19:33:48 +0100

    Oh

--- server/app/routes/user_api.py
+++ server/app/routes/user_api.py
@@ -7,13 +7,13 @@ from app import db
 from http import HTTPStatus
 from ..utils.helpers import make_log_wrapper
 
-user_bp = Blueprint('user_bp', __name__)
+user_bp = Blueprint("user_bp", __name__)
 
 # CREATE USER IN auth.py
 # PASSWORD RESET IN auth.py
 
 
-@user_bp.route('/api/current_user', methods=['GET'])
+@user_bp.route("/api/current_user", methods=["GET"])
 @jwt_required()
 def get_current_user():
     try:
@@ -24,17 +24,13 @@ def get_current_user():
         if user:
             return jsonify(user.to_dict()), HTTPStatus.OK
         else:
-            return jsonify({
-                "message": "User not found"
-            }), HTTPStatus.NOT_FOUND
+            return jsonify({"message": "User not found"}), HTTPStatus.NOT_FOUND
 
     except Exception as e:
-        return jsonify({
-            "message": str(e)
-        }), HTTPStatus.INTERNAL_SERVER_ERROR
+        return jsonify({"message": str(e)}), HTTPStatus.INTERNAL_SERVER_ERROR
 
 
-@user_bp.route('/api/user/del_user', methods=['DELETE'])
+@user_bp.route("/api/user/del_user", methods=["DELETE"])
 @jwt_required()
 @make_log_wrapper
 def del_user():
@@ -50,13 +46,16 @@ def del_user():
         return jsonify({"error": str(e)}), HTTPStatus.INTERNAL_SERVER_ERROR
 
 
-@user_bp.route('/api/user/update', methods=['POST'])
+@user_bp.route("/api/user/update", methods=["PUT"])
 @jwt_required()
 @make_log_wrapper
 def update_user():
     try:
-        user = get_jwt_identity()
+        user_id = get_jwt_identity()
         data = request.get_json()
+        user = User.query.get(user_id)
+        if not user:
+            return jsonify({"message": "User not found"}), HTTPStatus.NOT_FOUND
 
         update_user_fields(user, data)
         db.session.commit()
@@ -66,7 +65,7 @@ def update_user():
         return jsonify({"error": str(e)}), HTTPStatus.INTERNAL_SERVER_ERROR
 
 
-@user_bp.route('/api/user/admin/make_admin', methods=['POST'])
+@user_bp.route("/api/user/admin/make_admin", methods=["POST"])
 @jwt_required()
 @make_log_wrapper
 def make_admin():
@@ -82,13 +81,16 @@ def make_admin():
 
         target_user.admin = True
         db.session.commit()
-        return jsonify({"message": "User granted admin privileges successfully."}), HTTPStatus.OK
+        return (
+            jsonify({"message": "User granted admin privileges successfully."}),
+            HTTPStatus.OK,
+        )
 
     except Exception as e:
         return jsonify({"error": str(e)}), HTTPStatus.INTERNAL_SERVER_ERROR
 
 
-@user_bp.route('/api/user/admin/update', methods=['POST'])
+@user_bp.route("/api/user/admin/update", methods=["POST"])
 @jwt_required()
 @make_log_wrapper
 def update_user_by_admin():
@@ -104,7 +106,10 @@ def update_user_by_admin():
 
         update_user_fields(target_user, data)
         db.session.commit()
-        return jsonify({"message": "User granted admin privileges successfully."}), HTTPStatus.OK
+        return (
+            jsonify({"message": "User granted admin privileges successfully."}),
+            HTTPStatus.OK,
+        )
 
     except Exception as e:
         return jsonify({"error": str(e)}), HTTPStatus.INTERNAL_SERVER_ERROR
